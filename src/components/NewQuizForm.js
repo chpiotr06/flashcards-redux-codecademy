0c0ea1934df36b307577c6081fc74757243209a0
chpiotr06 <chpiotr06@gmail.com>
2022-07-21 09:47:33 +0200

    update repo

--- src/components/NewQuizForm.js
+++ src/components/NewQuizForm.js
@@ -6,6 +6,7 @@ import { useSelector, useDispatch } from "react-redux";
 import ROUTES from "../app/routes";
 import { selectTopics } from "../features/topics/topicsSlice"
 import { addQuizToTopicId } from "../features/quizzes/quizzesSlice";
+import { addCard } from "../features/cards/cardsSlice";
 
 export default function NewQuizForm() {
   const [name, setName] = useState("");
@@ -24,6 +25,12 @@ export default function NewQuizForm() {
 
     const cardIds = [];
 
+    cards.forEach((card) => {
+      let cardId = uuidv4();
+      cardIds.push(cardId);
+      dispatch(addCard({ ...card, id: cardId }));
+    });
+    
     // create the new cards here and add each card's id to cardIds
     // create the new quiz here
     const quizId = uuidv4();
